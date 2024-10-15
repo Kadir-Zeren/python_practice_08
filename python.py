45e2dfe446576859a1c910979abae8b8c33883ba
kadir-zeren <k4dzeren@gmail.com>
2024-10-15 19:04:18 +0300

    first commit

---
+++ python.py
@@ -0,0 +1,10 @@
+
+my_dict = {'key1': 'value1',
+'key2': 'value2',
+'key3': 'value3'
+}
+
+grocer1 = {'fruit':'apple', 'drink':'water'}
+grocer2 = dict(fruit='apple', drink='water')
+print(grocer1)
+print (grocer2)
